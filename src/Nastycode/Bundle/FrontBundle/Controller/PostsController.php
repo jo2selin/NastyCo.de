6015dcfa7959f0f2583c939d40aa08decaadd7e4
BlackSheepAgencyClem <hmich.c@gmail.com>
2015-03-16 16:12:16 +0100

    Add Comments

--- src/Nastycode/Bundle/FrontBundle/Controller/PostsController.php
+++ src/Nastycode/Bundle/FrontBundle/Controller/PostsController.php
@@ -8,7 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\SecurityContextInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-
+use Nastycode\Bundle\FrontBundle\Entity\Commentaires;
 
 class PostsController extends Controller
 {
@@ -16,7 +16,7 @@ class PostsController extends Controller
      * @Route("/nastycodes")
      * @Template()
      */
-    public function postsAction()
+    public function postsAction(Request $request)
     {
         $repository = $this
             ->getDoctrine()
@@ -25,12 +25,38 @@ class PostsController extends Controller
         ;
         $posts = $repository->findAll();
 
+        $commentaires = new Commentaires();
+
+        $comment = $this->get('form.factory')->createBuilder('form', $commentaires)
+            ->add('commentaires', 'textarea')
+            ->add('save',      'submit')
+            ->getForm()
+        ;
+
+        // On fait le lien Requête <-> Formulaire
+        // À partir de maintenant, la variable $commentaires contient les valeurs entrées dans le formulaire par le visiteur
+        $comment->handleRequest($request);
+
+        // On vérifie que les valeurs entrées sont correctes
+        // (Nous verrons la validation des objets en détail dans le prochain chapitre)
+        if ($comment->isValid()) {
+            // On l'enregistre notre objet $commentaires dans la base de données, par exemple
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($commentaires);
+            $em->flush();
+
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+
+            // On redirige vers la page de visualisation de l'annonce nouvellement créée
+            return $this->redirect($this->generateUrl('nastycode_comment_code', array('id' => $commentaires->getId())));
+        }
+
         $user = $this->getUser();
         return $this->render('NastycodeFrontBundle:Posts:posts.html.twig', array(
             'user' => $user,
             'posts' => $posts,
+            'comment' => $comment->createView(),
         ));
-
     }
 
 
@@ -53,4 +79,5 @@ class PostsController extends Controller
             'posts' => $posts,
         ));
     }   
+
 }
